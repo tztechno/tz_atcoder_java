75515dba8cd22f55c894b17ae5a130caed2807b9
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-26 22:11:58 +0900

    Update add_numbers.java

--- add_numbers.java
+++ add_numbers.java
@@ -1,3 +1,7 @@
+
+
+#####################
+
 public class Main {
     public static void main(String[] args) {
         int result = 1 + 1;
@@ -12,3 +16,21 @@ public class Main {
         System.out.println("Hello World!");
     }
 }
+
+#####################
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt();
+		int ans = (int)Math.pow(a,b);
+		System.out.print(ans);
+    }
+}
+#A,B=map(int,input().split())
+#print(A**B)
+
+
+#####################    
