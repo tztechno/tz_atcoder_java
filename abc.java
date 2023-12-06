a020e5167f1d471b435c9e23457c0f80b5b01207
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-06 20:41:44 +0900

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         double N = scanner.nextDouble();
-
         if (N <= 125) {
             System.out.println(4);
         } else if (N <= 211) {
@@ -13,7 +10,17 @@ public class Main {
         } else {
             System.out.println(8);
         }
-
         scanner.close();
+    }
+}
+
+################################################
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+         Scanner sc = new  Scanner(System.in);
+         int N = sc.nextInt();
+         System.out.println(N <= 125 ? 4 : N <= 211 ? 6 : 8);
     }
 }
