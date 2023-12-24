736ac830991e4733c9f2c9383ca1fbc5e46a821e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-24 16:01:53 +0900

    Update box.java

--- box.java
+++ box.java
@@ -1,10 +1,20 @@
-#ABC180_A
-#box
+//ABC180_A
+//box
 
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int x=sc.nextInt();
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       System.out.println(x-a+b);
+    }
+}
 
 
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,3 +24,19 @@ public class Main {
         System.out.println(N-A+B);
         }
     }
+
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int N, A, B;
+		N = input.nextInt();
+		A = input.nextInt();
+		B = input.nextInt();	
+		System.out.printf("%d", (N - A + B));
+	}
+}
+
+
+
