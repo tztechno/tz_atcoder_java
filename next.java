bd92ec49ab81bb0c6deaac97ae9e0e3a263815d9
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-24 19:04:16 +0900

    Create next.java

---
+++ next.java
@@ -0,0 +1,28 @@
+#ABC329_B
+#next
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; ++i) {
+            A[i] = scanner.nextInt();
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : A) {
+            set.add(num);
+        }
+
+        Integer[] uniqueArray = set.toArray(new Integer[0]);
+        Arrays.sort(uniqueArray);
+        System.out.println(uniqueArray[uniqueArray.length - 2]);
+        } 
+    }
