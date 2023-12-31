888bf5a7c22e75effba3d9690f2935f815f72d44
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-01 01:44:56 +0900

    Create not.java

---
+++ not.java
@@ -0,0 +1,18 @@
+//ABC178_A 
+//Not
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        if (N==1) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);            
+        }
+    }
+}
