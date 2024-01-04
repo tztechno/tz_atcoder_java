00e610a3231f23afdbc80f31aa08ca406c875e8b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-04 13:59:06 +0900

    Create late.java

---
+++ late.java
@@ -0,0 +1,52 @@
+//ABC177_A late
+
+###############################################
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int d = stdIn.nextInt();
+    int t = stdIn.nextInt();
+    int s = stdIn.nextInt();
+    
+    if(s * t >= d) System.out.println("Yes");
+    else System.out.println("No");
+  }
+}
+###############################################
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float D = scanner.nextFloat();
+        float T = scanner.nextFloat();
+        float S = scanner.nextFloat();
+        if (D / S <= T) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+###############################################
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int d = stdIn.nextInt();
+    int t = stdIn.nextInt();
+    int s = stdIn.nextInt();
+    
+    if(s * t >= d) System.out.println("Yes");
+    else System.out.println("No");
+  }
+}
+###############################################
+###############################################
