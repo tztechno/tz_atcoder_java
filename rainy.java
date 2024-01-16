03c79a4f852c8ff03e8376e592a7e4f224dcd917
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-16 11:55:35 +0900

    Update rainy.java

--- rainy.java
+++ rainy.java
@@ -1,5 +1,36 @@
 //abc175_a rainy.java
 
+############################################################
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        int count  = 0;
+        int maxCount = 0;
+        String s = in.nextLine();
+        for(int i = 0; i < 3;i++){
+            if(s.charAt(i)=='S'){
+                count  = 0;
+            }
+            else{
+                count++;
+                maxCount = count;
+            }
+        }
+        System.out.println(maxCount);
+    }
+}
+############################################################
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		System.out.println(S.contains("RRR")?3:S.contains("RR")?2:S.contains("R")?1:0);
+	}
+}
+############################################################
 import java.util.Scanner;
 
 public class Main {
@@ -26,3 +57,28 @@ public class Main {
         return max;
     }
 }
+############################################################
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        if (s.equals("RRR")) {
+            System.out.println(3);
+        } else if (s.contains("RR")) {
+            System.out.println(2);
+        } else if (s.contains("R")) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+}
+
+############################################################
