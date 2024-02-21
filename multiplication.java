371d6c819d91aa8b57fa47157f3edd4639a5b453
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-21 18:27:01 +0900

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,56 @@
+//abc169_a multiplication.java
+################################
+################################
+################################
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()*sc.nextInt());
+    }
+}
+################################
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a*b);
+    }
+}
+################################
+import java.util.*;
+class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int inline1 = sc.nextInt();
+    int inline2 = sc.nextInt();
+    int sum = inline1 * inline2;
+    System.out.print(sum);
+  }
+}
+################################
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A*B);
+    }
+}
+################################
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+    int[] X = new int[2];
+    for (int i=0; i<2; i++) {      
+        X[i] = stdIn.nextInt();
+      }
+    int ans=X[0]*X[1];
+    System.out.println(ans);                 
+      }   
+    }      
+################################
