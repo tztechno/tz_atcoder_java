98d9f0c1cc54d9ef4559045feae04f847763788b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-14 11:08:24 +0900

    Create practiceA.java

---
+++ practiceA.java
@@ -0,0 +1,66 @@
+//practiceA.java
+##################################
+1
+2 3
+test
+##################################
+##################################
+##################################
+##################################
+##################################
+##################################
+        System.out.print(a+b+c);
+        System.out.print(" ");
+        System.out.println(s);#at last
+##################################
+##################################
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int  a = sc.nextInt();
+        int  b = sc.nextInt();
+        int  c = sc.nextInt();
+        
+        String s = sc.next();
+        
+        System.out.print(a+b+c);
+        System.out.print(" ");
+        System.out.println(s);
+        
+    }
+}
+##################################
+[my favorite]
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+   Scanner s = new Scanner(System.in);
+   int a = s.nextInt();
+   int b = s.nextInt();
+   int c = s.nextInt();
+   String d = s.next();
+   int e = a + b + c;
+   System.out.println(e+" "+d);    
+  }  
+}
+##################################
+import java.util.Scanner;
+
+public class Main  {
+  
+  public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    String line = sc.next();
+    sc.close();
+    System.out.println((a+b+c) + " " + line);
+  }
+}
+##################################
