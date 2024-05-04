7ce7226ebcdefd5b5926d990102d259446a1af47
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-05-04 14:06:37 +0900

    Create abc186_a.java

---
+++ abc186_a.java
@@ -0,0 +1,33 @@
+abc186_a.java
+############################################
+############################################
+############################################
+############################################
+############################################
+############################################
+############################################
+############################################
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int W = sc.nextInt();
+        System.out.println(N/W);
+    }
+}
+############################################
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int w = sc.nextInt();
+    System.out.println(n/w);
+  }
+}
+############################################
+[python]
+n,w=map(int,input().split())
+print(n//w)
+############################################
